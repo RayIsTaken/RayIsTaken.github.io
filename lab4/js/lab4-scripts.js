040818aceb35b5b194faa39e57a3c4602cead8c0
Charles Delahousse <charles.delahousse@epita.fr>
2026-02-19 17:55:12 -0800

    renamed everything

---
+++ lab4/js/lab4-scripts.js
@@ -0,0 +1,58 @@
+let zipElement = document.querySelector("#zipCode");
+zipElement.addEventListener("change", displayCity);
+let passwordElement = document.querySelector("#passwordClick");
+passwordElement.addEventListener("click", suggestPassword);
+
+displayStates();
+async function displayStates()
+{
+    let url = "https://csumb.space/api/allStatesAPI.php";
+    try {
+        const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error("Error accessing API endpoint")
+            }
+        const data = await response.json();
+        console.log(data);
+        //alert(data[0].state);
+
+        for (let i of data){
+            let optionEl = document.createElement("option");
+            optionEl.textContent = i.state;
+            optionEl.value = i.usps;
+
+            document.querySelector("#state").append(optionEl);
+
+        }
+
+        } catch (err) {
+                if (err instanceof TypeError) {
+                    alert("Error accessing API endpoint (network failure)");
+                } else {
+                    alert(err.message);
+                }
+        } //catch    
+}
+
+async function displayCity()
+{
+    //alert("displaying city...")
+    let zipCode = zipElement.value;
+    let url = "https://csumb.space/api/cityInfoAPI.php?zip=" + zipCode;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    //alert(data.city);
+    document.querySelector("#city").textContent = data.city;
+    document.querySelector("#latitude").textContent = data.latitude;
+    document.querySelector("#longitude").textContent = data.longitude;
+}
+
+async function suggestPassword()
+{
+    let passwordSuggestedText = document.querySelector("#suggestPassword");
+    let suggestedPW = "https://csumb.space/api/suggestedPassword.php?length=8";
+    let response = await fetch(suggestedPW);
+    let data = await response.json();
+    passwordSuggestedText.textContent = "Suggested password : "+ data.password;
+}
